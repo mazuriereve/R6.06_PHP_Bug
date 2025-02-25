82e4ed433af4933c958d563ae03f6ef3426ac065
baron jules <jules.baron26@gmail.com>
2025-02-25 13:58:26 +0100

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS project_Jules_Clement;
+USE project_Jules_Clement;
+
+-- Table des utilisateurs
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+);
+
+-- Insertion d’utilisateurs
+INSERT INTO users (username, password) VALUES
+('admin', 'password123'),
+('user1', '123456');
